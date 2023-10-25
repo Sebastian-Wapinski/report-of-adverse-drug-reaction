f3ebbf1ae8b551556cffcae3f19ca35145d57d5f
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-10-25 23:14:13 +0200

    validation cr refactored

--- src/validation/validateForm.js
+++ src/validation/validateForm.js
@@ -1,3 +1,51 @@
+const checkMinAmountOfCharacters = (minAmountOfCharacters, valueLength) => {
+  return minAmountOfCharacters ? valueLength >= minAmountOfCharacters : true
+}
+
+const checkAmountOfCharacters = (amountOfCharacters, valueLength) => {
+  return amountOfCharacters ? valueLength === amountOfCharacters : true
+}
+
+const testRegExp = (regExp, value) => {
+  return regExp ? regExp.test(value) : true
+}
+
+const checkMinToValidate = (minToValidate, value) => {
+  return minToValidate === 0 ? value >= minToValidate : true
+}
+
+const checkMaxToValidate = (maxToValidate, value) => {
+  return maxToValidate ? value <= maxToValidate : true
+}
+
+const checkIsListIncludesText = (listElemArr, innerText) => {
+  return listElemArr ? listElemArr.includes(innerText) : true
+}
+
+const createList = (list) => {
+  return list.map(elem => {
+    return (
+      elem.resultType
+    )
+  })
+}
+
+const checkIsNotExceededCurrentDay = (currentDay, value, currentYear, currentMonth) => {
+  return currentDay ?
+      (Number(value.slice(6, 10)) < currentYear)
+        ||
+      (
+        Number(value.slice(3, 5)) < currentMonth
+        && Number(value.slice(6, 10)) <= currentYear
+      )
+        ||
+        (Number(value.slice(0, 2)) <= currentDay
+      && Number(value.slice(3, 5)) <= currentMonth
+      && Number(value.slice(6, 10)) <= currentYear)
+    :
+    true
+}
+
 export const validateForm = (formInputs, setValueForIsValid, formFieldData) => {
   const {
     name,
@@ -20,71 +68,23 @@ export const validateForm = (formInputs, setValueForIsValid, formFieldData) => {
 
   let listElemArr
   if (list) {
-    listElemArr = list.map(elem => {
-      return (
-        elem.resultType
-      )
-    })
+    listElemArr = createList(list)
   }
 
   if (
-    (
-      minAmountOfCharacters ?
-        value.length >= minAmountOfCharacters
-        :
-        true
-    )
+    checkMinAmountOfCharacters(minAmountOfCharacters, value.length)
           &&
-          (
-            amountOfCharacters ?
-              value.length === amountOfCharacters
-              :
-              true
-          )
+          checkAmountOfCharacters(amountOfCharacters, value.length)
           &&
-          (
-            regExp ?
-              regExp.test(value)
-              :
-              true
-          )
+          testRegExp(regExp, value)
           &&
-          (
-            minToValidate === 0 ?
-              value >= minToValidate
-              :
-              true
-          )
+          checkMinToValidate(minToValidate, value)
           &&
-          (
-            maxToValidate ?
-              value <= maxToValidate
-              :
-              true
-          )
+          checkMaxToValidate(maxToValidate, value)
           &&
-          (
-            listElemArr ?
-              listElemArr.includes(innerText)
-              :
-              true
-          )
+          checkIsListIncludesText(listElemArr, innerText)
           &&
-          (
-            currentDay ?
-                (Number(value.slice(6, 10)) < currentYear)
-               ||
-              (
-                Number(value.slice(3, 5)) < currentMonth
-                && Number(value.slice(6, 10)) <= currentYear
-              )
-              ||
-                (Number(value.slice(0, 2)) <= currentDay
-              && Number(value.slice(3, 5)) <= currentMonth
-              && Number(value.slice(6, 10)) <= currentYear)
-              :
-              true
-          )
+          checkIsNotExceededCurrentDay(currentDay, value, currentYear, currentMonth, currentDay)
   ) {
     setValueForIsValid({ pageName, name: [name + 'IsValid'], value: true })
   } else {
